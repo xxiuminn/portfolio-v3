ae9543c4003026858a0e444e12a028766370e518
xxiuminn <xiumin1997@gmail.com>
2026-06-19 00:06:53 +0800

    change description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Xiumin How",
-  description: "Building and shipping products.",
+  description: "Performance Marketing Connoisseur",
 };
 
 export default function RootLayout({
@@ -18,28 +18,28 @@ export default function RootLayout({
       <body>
         <CustomCursor />
         <div className="site-nav-wrapper">
-        <nav className="site-nav">
-          <Link href="/" className="site-nav-name">
-            Xiumin How
-          </Link>
-          <div className="site-nav-links">
-            <a href="mailto:xiumin.how.mail@gmail.com">Email</a>
-            <a
-              href="https://www.linkedin.com/in/howxiumin/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              LinkedIn
-            </a>
-            <a
-              href="https://github.com/xxiuminn"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github
-            </a>
-          </div>
-        </nav>
+          <nav className="site-nav">
+            <Link href="/" className="site-nav-name">
+              Xiumin How
+            </Link>
+            <div className="site-nav-links">
+              <a href="mailto:xiumin.how.mail@gmail.com">Email</a>
+              <a
+                href="https://www.linkedin.com/in/howxiumin/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://github.com/xxiuminn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </div>
+          </nav>
         </div>
         {children}
       </body>
